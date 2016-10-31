c3a265d04132a102ee14ce0d20f348620e532ed6
zscseh <zscseh93@gmail.com>
2016-10-31 13:11:06 +0100

    Created initial skeleton of menus.

---
+++ Client/MainMenu.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClickCreateGameButton(object sender, RoutedEventArgs e)
+        {
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new CreateLobby());
+        }
+
+        private void OnClickConnectToLobbyButton(object sender, RoutedEventArgs e)
+        {
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new ConnectLobby());
+        }
+
+        private void OnClickStatisticsButton(object sender, RoutedEventArgs e)
+        {
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new Statistics());
+        }
+
+        private void OnClickSettingsButton(object sender, RoutedEventArgs e)
+        {
+            NavigationService navigationService = NavigationService.GetNavigationService(this);
+            navigationService.Navigate(new Settings());
+        }
+
+        private void OnClickQuitButton(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
