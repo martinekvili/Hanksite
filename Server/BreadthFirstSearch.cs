c94e661d9ba5ea38d3b2bbdec197d0e17c2456bf
vmartinek <martinek.vilmos@mirango.hu>
2016-11-07 01:26:38 +0100

    Implemented BFS

---
+++ Server/BreadthFirstSearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class BreadthFirstSearch
+    {
+        public static List<Hexagon> Search(Hexagon startCell, Func<Hexagon, bool> isCellMatching, bool perimeterOnly)
+        {
+            List<Hexagon> matchingCells = new List<Hexagon>();
+
+            HashSet<Hexagon> alreadyVisited = new HashSet<Hexagon>();
+            Queue<Hexagon> bfsQueue = new Queue<Hexagon>();
+            bfsQueue.Enqueue(startCell);
+
+            while (bfsQueue.Count != 0)
+            {
+                Hexagon cell = bfsQueue.Dequeue();
+                alreadyVisited.Add(cell);
+
+                if (!isCellMatching(cell))
+                {
+                    if (perimeterOnly)
+                        matchingCells.Add(cell);
+
+                    continue;
+                }
+
+                if (!perimeterOnly)
+                    matchingCells.Add(cell);
+
+                foreach (var neighbour in cell.Neighbours)
+                {
+                    if (alreadyVisited.Contains(neighbour))
+                        continue;
+
+                    bfsQueue.Enqueue(neighbour);
+                }
+            }
+
+            return matchingCells;
+        }
+    }
+}
