31a1f3cb2b032e0b9f9d704b6c8e92b59f9e9808
vmartinek <martinek.vilmos@mirango.hu>
2016-11-17 22:52:22 +0100

    Removed unneeded junk file created by merge

--- Server.DAL/HanksiteModel.Designer.cs
+++ Server.DAL/HanksiteModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\Hanksite\Server.DAL\HanksiteModel.edmx'. 
+﻿// T4 code generation is enabled for model 'HanksiteModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
